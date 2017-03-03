779939f7913ebbfc462e5eea37eaf773e08a4c38
Choffa <cgjovag@gmail.com>
2017-03-03 10:09:00 +0100

    Added some JavaDoc for class and constructor

--- backend/src/backend/TeacherInfo.java
+++ backend/src/backend/TeacherInfo.java
@@ -2,10 +2,25 @@ package backend;
 
 import java.io.Serializable;
 
+/**
+ * A class for the information that the professor will recieve from the server backend
+ * 
+ * @author Mathias, documentation by choffa
+ *
+ */
 public class TeacherInfo implements Serializable{
-    float snitt;
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	float snitt;
     int antall;
 
+    /**
+     * Constructs a new TeacherInfo object, obviously
+     * @param The current average rating
+     * @param The number or student that have rated
+     */
     public TeacherInfo(float snitt, int antall) {
         this.snitt = snitt;
         this.antall = antall;
