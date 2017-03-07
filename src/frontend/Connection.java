1010a17e2a6097631029873ccd2bfdc2910a6c8c
Choffa <cgjovag@gmail.com>
2017-03-07 13:14:33 +0100

    more changes to connection.java

--- src/frontend/Connection.java
+++ src/frontend/Connection.java
@@ -4,6 +4,7 @@ import java.io.*;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Scanner;
 import java.time.LocalDate;
 import java.time.LocalTime;
@@ -11,17 +12,18 @@ import java.time.LocalTime;
 public class Connection implements Closeable, AutoCloseable {
 
 	private final int PORT = 4728;
-	private final String HOST = "localhost";	
+	private final String HOST = "localhost";
 	private Socket socket;
 	private PrintWriter out;
 	private Scanner in;
 	private boolean isClosed;
-	
+
 	//------------------------------------------------------------------------
 	//The connection stuff
-	
+
 	/**
 	 * A method that sets up the connection to the server
+	 *
 	 * @throws IOException
 	 */
 	public Connection() throws IOException {
@@ -30,13 +32,17 @@ public class Connection implements Closeable, AutoCloseable {
 		out = new PrintWriter(socket.getOutputStream());
 		in = new Scanner(socket.getInputStream());
 	}
-	
+
 	/**
 	 * A method that closes the connection to the server
+	 *
 	 * @throws IOException
 	 */
 	@Override
 	public void close() throws IOException {
+		if (isClosed) {
+			return;
+		}
 		isClosed = true;
 		out.println("CLOSE");
 		out.flush();
@@ -47,96 +53,115 @@ public class Connection implements Closeable, AutoCloseable {
 
 	/**
 	 * A method that checks is the connection has been closed
-	 * @return The result of Socket.isclosed()
+	 *
+	 * @return True if the connection has been closed
 	 */
-	public boolean isClosed(){
+	public boolean isClosed() {
 		return this.isClosed;
 	}
-	
+
 	//------------------------------------------------------------------------
 	//The subject rating stuff
-	
+
 	/**
 	 * A method for sending a subject rating to the server
+	 *
 	 * @param subjectID The ID of the subject that is to be rated
 	 * @param studentID The ID of the student that sent this rating
-	 * @param rating The actual rating of the subject, this is a number between 1 and 5 (inclusive)
+	 * @param rating    The actual rating of the subject, this is a number between 1 and 5 (inclusive)
 	 * @throws IllegalArgumentException This is thrown if the ranking is wrong
-	 * @throws IllegalStateException This is thrown if the connection isn't active
 	 */
-	public void sendSubjectRating(int subjectID, int studentID, int rating, String comment) 
-			throws IllegalArgumentException, IllegalStateException {
-		if (isClosed | !socket.isConnected()){
-			throw new IllegalStateException("Socket is not connected, please run Connection.connect() first");
-		}
-		if (rating < 1 || rating > 5){
+	public void sendSubjectRating(int subjectID, int studentID, int rating, String comment)
+			throws IllegalArgumentException {
+		checkState();
+		if (rating < 1 || rating > 5) {
 			throw new IllegalArgumentException("Rating must be between 1 and 5");
 		}
-		
+
 		out.println("SET_SUBJECTRATING " + subjectID + " " + studentID + " " + rating + " " + comment);
 		out.flush();
 	}
-	
+
 	/**
-	 * A method that requests the subjectRating from a specific subject 
+	 * A method that requests the subjectRating from a specific subject
+	 *
 	 * @param subjectID The subjectID of the subject in question
 	 * @return A float that represents the average rating for the subject
 	 */
-	public float getAverageSubjectRating(int subjectID){
+	public float getAverageSubjectRating(int subjectID) {
+		checkState();
 		out.println("GET_AVERAGESUBJECTRATING");
 		out.flush();
 		return in.nextFloat();
 	}
-	
+
 	//------------------------------------------------------------------------
 	//The lecture stuff
-	
+
 	/**
 	 * A method that request the currently active lectures
+	 *
 	 * @return An ArrayList with the lectures in it
 	 */
-	public ArrayList<Object> getLectures(){
+	public ArrayList<Lecture> getLectures() {
 		//TODO: Create method for receiving the lectures from the server
+		checkState();
+		out.println("GET_ALLLECTURES");
+		out.flush();
+		ArrayList<Lecture> lectures = new ArrayList<>();
+		String resString = in.nextLine();
+		String[] resArray = resString.split("#");
+		for (String s : resArray){
+			String[] lectureArray = s.split(" ");
+			lectures.add(new Lecture(Integer.parseInt(lectureArray[0]),
+					lectureArray[1], lectureArray[2], new Date(lectureArray[3]),
+					Integer.parseInt(lectureArray[4]), Integer.parseInt(lectureArray[5]), lectureArray[6] ));
+		}
+		System.out.println(lectures);
 		return null;
 	}
 
 	/**
 	 * A method that request the lectures by a specific professor
+	 *
 	 * @param professorID The ID of the professor that held the lectures
 	 * @return An ArrayList with the lectures in it
 	 */
-	public ArrayList<Object> getLectures(int professorID){
+	public ArrayList<Object> getLectures(int professorID) {
 		//TODO: Create method for getting lectures by specific professor
 		return null;
 	}
-	
+
 	/**
 	 * A method for creating a new lecture in the database
-	 * @param date The date that the lecture takes place
+	 *
+	 * @param date  The date that the lecture takes place
 	 * @param start The time the lecture starts
-	 * @param end The end time the lecture ends
-	 * @param room The room that the lecture takes place
+	 * @param end   The end time the lecture ends
+	 * @param room  The room that the lecture takes place
 	 */
-	public void createLecture(int professorID, LocalDate date, LocalTime start, LocalTime end, String room){
+	public void createLecture(int professorID, LocalDate date, LocalTime start, LocalTime end, String room) {
 		//TODO: Make method body for 
 	}
-	
+
 	//------------------------------------------------------------------------
 	//The speed rating stuff
-	
+
 	/**
 	 * A method for sending a speed rating to the server
+	 *
 	 * @param lectureID The ID of the lecture being rated on
 	 * @param studentID The ID of the student performing the rating
-	 * @param rating The actual rating of the speed. This should be a number between 1 and 5 (inclusive)
+	 * @param rating    The actual rating of the speed. This should be a number between 1 and 5 (inclusive)
 	 * @throws IllegalArgumentException if the rating is not between 1 and 5
 	 */
-	public void sendSpeedRating(int lectureID, int studentID, int rating) throws IllegalArgumentException{
+	public void sendSpeedRating(int lectureID, int studentID, int rating) throws IllegalArgumentException {
 		//TODO: Create method for sending speed-rating
 	}
-	
+
 	/**
 	 * A method that request the speed rating for specific lecture
+	 *
 	 * @param lectureID The ID of the lecture to get the rating for
 	 * @return A float representing the current average speed rating
 	 */
@@ -144,12 +169,13 @@ public class Connection implements Closeable, AutoCloseable {
 		//TODO: Create method for getting speed-rating for specific lecture
 		return 0;
 	}
-	
+
 	//------------------------------------------------------------------------
 	//the subject stuff
-	
+
 	/**
 	 * A method that requests the subjects associated with a specific lecture
+	 *
 	 * @param lectureID The ID of the lecture to get subjects for
 	 * @return A ArrayList of integers representing the ID of the subjects
 	 */
@@ -157,13 +183,20 @@ public class Connection implements Closeable, AutoCloseable {
 		//TODO: Create method for getting the subjects for specific lectures
 		return null;
 	}
-	
+
 	/**
 	 * A method for creating a new subject associated to a lecture in the database
+	 *
 	 * @param lectureID The ID of the lecture to associate the subject with
 	 */
 	public void createSubject(int lectureID) {
 		//TODO: Create method for creating subject associated with specific lecture
 	}
-	
+
+	private void checkState() {
+		if(this.isClosed |!socket.isConnected()){
+			throw new IllegalStateException();
+		}
+
+	}
 }
