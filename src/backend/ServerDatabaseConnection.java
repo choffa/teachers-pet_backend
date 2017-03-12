fc4daea37e683d46383deb6ffc9f2ff98bdbc755
mathilie <mlien1995@gmail.com>
2017-03-12 14:46:19 +0100

    ServerDatabase snakker med IDI databasen
    -La til finals for lecture, speedranking etc
    -la til en Databasetester class
    -SDC prater nå med databasen.

--- src/backend/ServerDatabaseConnection.java
+++ src/backend/ServerDatabaseConnection.java
@@ -2,36 +2,84 @@ package backend;
 
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import com.mysql.jdbc.Connection;
+
+import java.sql.Statement;
+
+import java.sql.Connection;
 
 
 public class ServerDatabaseConnection {
-public void connect(){
-Connection con  =  null;
-try {
-  Class.forName("com.mysql.jdbc.Driver").newInstance();
-  String url = "jdbc:mysql://mysql.idi.ntnu.no/mindatabase";
-  String user = "brukernavn";
-  String pw = "passord";
-  con = DriverManager.getConnection(url,user,pw);
-  System.out.println("Tilkoblingen fungerte.");
-  } catch (SQLException ex) {
-    System.out.println("Tilkobling feilet: "+ex.getMessage());
-  } catch (ClassNotFoundException ex) {
-    System.out.println("Feilet under driverlasting: "+ex.getMessage());
-  } catch (InstantiationException e) {
-	// TODO Auto-generated catch block
-	e.printStackTrace();
-} catch (IllegalAccessException e) {
-	// TODO Auto-generated catch block
-	e.printStackTrace();
-} finally {
-    try {
-      if (con !=  null) con.close();
-    } catch (SQLException ex) {
-      System.out.println("Epic fail: "+ex.getMessage());
-    }
-  }
-con.
-  }
+	private Connection con  =  null;
+	private String url = "jdbc:mysql://mysql.idi.ntnu.no/p_teacherspet";
+	private String user = "tpbackend_adm";
+	private String pw = "ang4OhmieD7aefo";
+	
+	public static final String LECTURES = "Lectures(LectureID,LectureDate,StartTime,EndTime,Professor)";
+	public static final String SUBJECTS = "Subjects(LectureID,SubjectID,SubjectName)";
+	public static final String SUBJECTRANKING = "SubjectRanking(Ranking,RankingComment,SubjectID,StudentID)";
+	public static final String SPEEDRANKING = "SpeedRanking(LectureID,Ranking,StudentID)";
+	
+	
+	public void insert(String tableName, String[] args){
+		try{
+			connect();
+			Statement s = con.createStatement();
+			String values = "(";
+			for (String arg:args) values+=arg+",";
+			values = values.substring(0, values.length()-1);
+			String query = "INSERT INTO "+tableName+" VALUES "+values+");";
+			System.out.println(query);
+			s.execute(query);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			System.out.println("insert command failed");
+			e.printStackTrace();
+		}finally{
+			close();
+		}
+	}
+	
+	
+	
+	
+	public boolean testConnection(){
+			connect();
+			close();
+		return true;
+	}
+	
+	private void connect(){
+		try {
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+			con = DriverManager.getConnection(url,user,pw);
+			System.out.println("Tilkoblingen fungerte.");
+			  } catch (SQLException ex) {
+			    System.out.println("Tilkobling feilet: "+ex.getMessage());
+			  } catch (ClassNotFoundException ex) {
+			    System.out.println("Feilet under driverlasting: "+ex.getMessage());
+			  } catch (InstantiationException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			  } catch (IllegalAccessException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			  } 
+	}
+	
+	private void close(){
+		{
+		    try {
+		      if (con !=  null) con.close();
+		    } catch (SQLException ex) {
+		      System.out.println("Epic fail: "+ex.getMessage());
+		    }
+		  }
+	}
+	
+	public static void main(String[] args) {
+		ServerDatabaseConnection sdc = new ServerDatabaseConnection();
+		sdc.testConnection();
+		sdc.insert(ServerDatabaseConnection.SUBJECTS, new String[] {"1", "2", "'testName'"});
+	}
+
 }
