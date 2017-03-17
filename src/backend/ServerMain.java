fd212cd6f9e3e94a8ce05d6ff46802f8d99a273e
mathilie <mlien1995@gmail.com>
2017-03-17 16:02:35 +0100

    Made ServerMain main method.

--- src/backend/ServerMain.java
+++ src/backend/ServerMain.java
@@ -7,7 +7,10 @@ import java.net.Socket;
 public class ServerMain {
 
 	public static void main(String[] args) {
-		
+		ServerMain main = new ServerMain();
+		main.init();
+		System.out.println("Server starting");
+		main.run();
 	}
 	
 	private final int PORT = 4728;
@@ -24,9 +27,11 @@ public class ServerMain {
 	}
 	
 	protected void run(){
+		System.out.println("Server started");
 		while (true){
 			try {
 				client = ss.accept();
+				System.out.println("Connection accepted");
 				new Thread(new ServerConnection(client)).start();
 				client.close();
 			} catch (IOException e) {
