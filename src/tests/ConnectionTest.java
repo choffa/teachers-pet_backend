34cf5ab97563b807c4a099b1f68414f30ccc08eb
Choffa <cgjovag@gmail.com>
2017-03-31 12:09:53 +0200

    Finished test

--- src/tests/ConnectionTest.java
+++ src/tests/ConnectionTest.java
@@ -2,9 +2,11 @@ package tests;
 
 import frontend.Connection;
 import frontend.Lecture;
+import frontend.Subject;
 import org.junit.*;
 
 import java.io.*;
+import java.net.InterfaceAddress;
 import java.net.Socket;
 import java.sql.Date;
 import java.util.ArrayList;
@@ -18,21 +20,60 @@ import static org.mockito.Mockito.*;
  */
 public class ConnectionTest {
 
-    Connection c;
-    Socket s;
-    InputStream in;
-    OutputStream out;
+    private Connection c;
+    private Socket s;
+    private InputStream in;
+    private OutputStream out;
 
-    double average;
-    int lectureID;
+    private static double average;
+    private static int lectureID;
+    private static int subjectID;
+    private static int rating;
+    private static int badRating;
+    private static int ratings;
 
-    String averageString;
+    private static String averageString;
+    private static String lectureIDString;
+    private static String ratingsString;
 
-    @Before
-    public void setUp() throws IOException {
+    private static final String userName = "djfsakløfjkløsjaklfø";
+    private static final String professorID = "djfsakløfjkløsjaklfø";
+    private static final String password = "trwuiodsjoivdsgl325u485";
+    private static final String studentID = "kdigfur64990#jdfydkwmdur&&jkd/daeb";
+    private static final String lectureResponse = "END";
+    private static final String subjectResponse = "END";
+    private static final String subjectName = "EULERS_THEROEM";
+    private static final String badSubjectName = "EULERS THEOREM";
+    private static final String comment = "THISISNICE";
+    private static final String room = "R4";
+    private static final String badRoom = "R4 134";
+    private static final String courseID = "TDT4100";
+    @SuppressWarnings("deprecation")
+    private static final Date date = new Date(120, 1, 1);
+    private static final int start = 12;
+    private static final int end = 17;
+
+    @BeforeClass
+    public static void setUpClass() {
+        Random r = new Random();
+        average = r.nextDouble() * 5.0;
+        lectureID = r.nextInt();
+        subjectID = r.nextInt();
+        ratings = r.nextInt(300);
+        do{
+            rating = r.nextInt(6);
+        }while(rating < 1);
+        do{
+            badRating = r.nextInt();
+        }while(badRating > 0 && badRating < 6);
+        averageString = Double.toString(average);
+        lectureIDString = Integer.toString(lectureID);
+        ratingsString = Integer.toString(ratings);
+    }
+
+    public void setUp(String response) throws IOException {
         s = mock(Socket.class);
-        String responses = "1.54 END END 4.355";
-        byte[] b = responses.getBytes();
+        byte[] b = response.getBytes();
         in = new ByteArrayInputStream(b);
         out = new ByteArrayOutputStream();
         when(s.getOutputStream()).thenReturn(out);
@@ -40,36 +81,12 @@ public class ConnectionTest {
         when(s.isConnected()).thenReturn(true);
         c = new Connection(s);
 
-        Random r = new Random();
-        average = r.nextDouble() * 5.0;
-        lectureID = r.nextInt();
-
-        averageString = Double.toString(average);
     }
+
 
     @Test
-    public void createSubject() {
-        int lectureID = 12;
-        String name = "EULERS_THEOREM";
-        String expectedCommands = "SET_SUBJECT " + lectureID + " " + name + "\r\n";
-        c.createSubject(lectureID, name);
-
-        assertEquals(expectedCommands, out.toString());
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void badNameCreateSubject() {
-        int lectureID = 12;
-        String name = "EULERS THEOREM";
-        c.createSubject(lectureID, name);
-    }
-
-    @Test
-    public void sendSubjectRating() {
-        int subjectID = 123;
-        String studentID = "fhkdsjlfhk";
-        int rating = 5;
-        String comment = "THISISNICE";
+    public void sendSubjectRating() throws IOException {
+        setUp("");
         String expectedCommand = "SET_SUBJECTRATING " + subjectID +  " " + studentID + " " + rating + " " + comment +"\r\n";
         c.sendSubjectRating(subjectID, studentID, rating, comment);
 
@@ -77,42 +94,35 @@ public class ConnectionTest {
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void badRatingSendSubjectRating() {
-        int subjectID = 123;
-        String studentID = "fhkdsjlfhk";
-        int rating = 100;
-        String comment = "THIS IS NOT NICE";
-        String expectedCommand = "SET_SUBJECTRATING " + subjectID +  " " + studentID + " " + rating + " " + comment +"\r\n";
-        c.sendSubjectRating(subjectID, studentID, rating, comment);
-
-        assertEquals(expectedCommand, out.toString());
+    public void badRatingSendSubjectRating() throws IOException {
+        setUp("");
+        c.sendSubjectRating(subjectID, studentID, badRating, comment);
     }
 
     @Test
-    public void getAverageSubjectRating() {
-        int subjectID = 1234;
+    public void getAverageSubjectRating() throws IOException {
+        setUp(averageString);
         String expectedCommands = "GET_AVERAGESUBJECTRATING " + subjectID + "\r\n";
         double rating = c.getAverageSubjectRating(subjectID);
-        double expectedResponse = 1.54;
 
         assertEquals(expectedCommands, out.toString());
-        assertEquals(expectedResponse, rating, 0.001);
+        assertEquals(average, rating, 0.001);
     }
 
+    //TODO: these test needs fixing
     @Test
-    public void getNoLectures() {
-        String expectedCommans = "GET_ALLLECTURES\r\n";
+    public void getLectures() throws IOException {
+        setUp(lectureResponse);
+        String expectedCommands = "GET_ALLLECTURES\r\n";
         ArrayList<Lecture> lectureList = c.getLectures();
 
-        assertEquals(expectedCommans, out.toString());
+        assertEquals(expectedCommands, out.toString());
         assertTrue(lectureList.isEmpty());
     }
 
-    //TODO: Add test with actual lecture input
-
     @Test
-    public void getNoLecturesFromProfessorID () {
-        String professorID = "jfkdsløafjkdslø";
+    public void getLecturesFromProfessorID () throws IOException {
+        setUp(lectureResponse);
         String expectedCommands = "GET_LECTURE " + professorID + "\r\n";
         ArrayList<Lecture> lectureList = c.getLectures(professorID);
 
@@ -120,17 +130,11 @@ public class ConnectionTest {
         assertTrue(lectureList.isEmpty());
     }
 
-    //TODO: Add test with actual lecture input
+    //TODO: END TODO
 
     @Test
-    public void createLecture() {
-        String professorID = "abcdefghijklmnopqrst";
-        String courseID = "TDT4140";
-        @SuppressWarnings("deprecation")
-        Date date = new Date(120, 1, 1);
-        int start = 12;
-        int end = 14;
-        String room = "R42";
+    public void createLecture() throws IOException {
+        setUp("");
         String expectedCommands = "SET_LECTURE " + professorID + " " + courseID + " " + date + " " + start + " " + end + " " + room + "\r\n";
         c.createLecture(professorID, courseID, date, start, end, room);
 
@@ -138,26 +142,14 @@ public class ConnectionTest {
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void badRoomCreateLecture() {
-        String professorID = "abcdefghijklmnopqrst";
-        String courseID = "TDT4140";
-        @SuppressWarnings("deprecation")
-        Date date = new Date(120, 1, 1);
-        int start = 12;
-        int end = 14;
-        String room = "R42 fjkdsløa";
-        c.createLecture(professorID, courseID, date, start, end, room);
+    public void badRoomCreateLecture() throws IOException {
+        setUp("");
+        c.createLecture(professorID, courseID, date, start, end, badRoom);
     }
 
     @Test
-    public void createLectureFromObject() {
-        String professorID = "abcdefghijklmnopqrst";
-        String courseID = "TDT4140";
-        @SuppressWarnings("deprecation")
-        Date date = new Date(120, 1, 1);
-        int start = 12;
-        int end = 14;
-        String room = "R42";
+    public void createLectureFromObject() throws IOException {
+        setUp("");
         Lecture lecture = new Lecture(-1, professorID, courseID, start, end, room, date);
         String expectedCommands = "SET_LECTURE " + professorID + " " + courseID + " " + date + " " + start + " " + end + " " + room + "\r\n";
         c.createLecture(lecture);
@@ -166,10 +158,8 @@ public class ConnectionTest {
     }
 
     @Test
-    public void sendSpeedRating() {
-        int lectureID = 13245;
-        String studentID = "jfkdsløafjaklø3402##";
-        int rating = 3;
+    public void sendSpeedRating() throws IOException {
+        setUp("");
         String expectedCommands = "SET_SPEEDRATING " + lectureID + " " + rating + " " + studentID + "\r\n";
         c.sendSpeedRating(lectureID, studentID, rating);
 
@@ -177,22 +167,14 @@ public class ConnectionTest {
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void badRatingSendSpeedRating() {
-        int lectureID = 123445;
-        String studentID = "jfkdsløfjdskløfsaQ";
-        int rating = 12;
-        String expectedCommands = "SET_SPEEDRATING " + lectureID + " " + studentID + " " + rating + "\r\n";
-        c.sendSpeedRating(lectureID, studentID, rating);
+    public void badRatingSendSpeedRating() throws IOException {
+        setUp("");
+        c.sendSpeedRating(lectureID, studentID, badRating);
     }
 
     @Test
-    public void testGetAverageSpeedRating() throws IOException {
-        String response = averageString;
-        setUp(response);
-        getAverageSpeedRating();
-    }
-
-    private void getAverageSpeedRating() {
+    public void getAverageSpeedRating() throws IOException {
+        setUp(averageString);
         String expectedCommands = "GET_AVERAGESPEEDRATING " + lectureID + "\r\n";
         double response = c.getAverageSpeedRating(lectureID);
 
@@ -200,24 +182,81 @@ public class ConnectionTest {
         assertEquals(average, response, 0.01);
     }
 
+    //TODO: must be fixed
     @Test
-    public void allCommandsAtOnce() throws IOException {
-        String responses = averageString;
-        setUp(responses);
-        getAverageSpeedRating();
+    public void getSubjects() throws IOException {
+        setUp(subjectResponse);
+        String expectedCommands = "GET_SUBJECTS\r\n";
+        ArrayList<Subject> response = c.getSubjects(lectureID);
+
+        assertEquals(expectedCommands, out.toString());
+        assertTrue(response.isEmpty());
     }
 
-    private void setUp(String response) throws IOException {
-        //TODO: Remove once methods have been rewritten
-        c.close();
-        
-        byte[] b = response.getBytes();
-        in = new ByteArrayInputStream(b);
-        out = new ByteArrayOutputStream();
-        when(s.getOutputStream()).thenReturn(out);
-        when(s.getInputStream()).thenReturn(in);
-        when(s.isConnected()).thenReturn(true);
-        c = new Connection(s);
+    @Test
+    public void createSubject() throws IOException {
+        setUp("");
+        String expectedCommands = "SET_SUBJECT " + lectureID + " " + subjectName + "\r\n";
+        c.createSubject(lectureID, subjectName);
+
+        assertEquals(expectedCommands, out.toString());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void badNameCreateSubject() throws IOException {
+        setUp("");
+        c.createSubject(lectureID, badSubjectName);
+    }
+
+    @Test
+    public void validateUser() throws IOException {
+        setUp("true false");
+        String expectedCommands = "VALIDATE " + userName + " " + password + "\r\n";
+        boolean response = c.validateUser(userName, password);
+
+        assertEquals(expectedCommands, out.toString());
+        assertTrue(response);
+
+        expectedCommands = expectedCommands + expectedCommands;
+        response = c.validateUser(userName, password);
+
+        assertEquals(expectedCommands, out.toString());
+        assertFalse(response);
+    }
+
+    @Test
+    public void createUser() throws IOException {
+        setUp("");
+        String expectedCommands = "SET_USER " + userName + " " + password + "\r\n";
+        c.createUser(userName, password);
+
+        assertEquals(expectedCommands, out.toString());
+    }
+
+    @Test
+    public void checkUserName() throws IOException {
+        setUp("false true");
+        String expectedCommands = "CHECK_USER " + userName + "\r\n";
+        boolean response = c.checkUsername(userName);
+
+        assertEquals(expectedCommands, out.toString());
+        assertFalse(response);
+
+        expectedCommands = expectedCommands + expectedCommands;
+        response = c.checkUsername(userName);
+
+        assertEquals(expectedCommands, out.toString());
+        assertTrue(response);
+    }
+
+    @Test
+    public void getTempoVotesInLecture() throws IOException {
+        setUp(ratingsString);
+        String expectedCommands = "GET_NUMBEROFUSERS " + lectureID + "\r\n";
+        int response = c.getTempoVotesInLecture(lectureID);
+
+        assertEquals(expectedCommands, out.toString());
+        assertEquals(ratings, response);
     }
 
     @After
