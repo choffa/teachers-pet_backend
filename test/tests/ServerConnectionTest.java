94346a946987e15e2dfbdb3beec3e4df214d9fd0
mathilie <mlien1995@gmail.com>
2017-04-01 17:59:01 +0200

    midlertidig

--- test/tests/ServerConnectionTest.java
+++ test/tests/ServerConnectionTest.java
@@ -4,20 +4,26 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import com.sun.jmx.snmp.internal.SnmpAccessControlModel;
+
 import backend.ServerConnection;
 import backend.ServerDatabaseConnection;
+import frontend.Connection;
 
 import static org.junit.Test.*;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.ObjectOutputStream;
 import java.io.OutputStream;
 import java.io.PipedInputStream;
 import java.io.PipedOutputStream;
 import java.io.PrintWriter;
 import java.io.StringBufferInputStream;
 import java.net.Socket;
+import java.util.Random;
 import java.util.stream.Stream;
 
 import static org.junit.Before.*;
@@ -31,20 +37,31 @@ public class ServerConnectionTest {
     private ServerConnection sc;
     private ServerDatabaseConnection sdc;
     private Socket s;
-    private OutputStream out;
-    private InputStream in;
+    private ByteArrayOutputStream out;
+    private ByteArrayInputStream in;
     private PrintWriter p;
+    private String serverReturn = "1.54 END END 4.355";
     
     
     @Before
     public void init() throws IOException {
-    	p = new PrintWriter(out);
     	s = mock(Socket.class);    	
     	when(s.getOutputStream()).thenReturn(out);
     	when(s.getInputStream()).thenReturn(in);
+    	when(s.isConnected()).thenReturn(true);
     	sdc = mock(ServerDatabaseConnection.class);
     	sc = new ServerConnection(s, sdc);
+    	when(sdc.testConnection()).thenReturn(true);
+    	
+    	byte[] b = serverReturn.getBytes();
     }
+    
+    public void test2(){
+    	
+    	
+    	
+    }
+
     
     @Test
     public void closeTest() throws NumberFormatException, IOException{
